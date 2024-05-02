66b7af46eb7e072dfda2a1c1ebb3c21038863c43
Guerlain <guerlain.hitier@gmail.com>
2024-05-02 02:33:58 +0200

    Fix input validation and centered board

--- Script.js
+++ Script.js
@@ -9,6 +9,7 @@ var coloredTiles = [];
 var outputy = document.getElementById('outputy');
 var outputx = document.getElementById('outputx');
 var output = document.getElementById('output');
+var allowedInputs = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 for (var i = 0; i < 9; i++) {
     //get current inner div
@@ -33,16 +34,21 @@ for (var i = 0; i < 9; i++) {
             var col = getCol(x);
 
             
+            checkInput(event, row, col, grid);
 
-            if(!checkValid(event.target, row, col, grid)){
-                event.target.value = '';
-            }
         });
         counter++;
     }
 }
 
-
+function checkInput(event, row, col, grid) {
+    
+    //check if input is valid (1-9) or valid sudoku move if invalid, set to nothing
+    if(!allowedInputs.includes(event.target.value) || !checkValidMove(event.target, row, col, grid)) {
+        event.target.value = '';
+        return;
+    }
+}
 
 function getRow(x) {
     return board[x];
@@ -83,26 +89,40 @@ function getGrid(x, y) {
     return grid;
 }
 
-function checkValid(target, row, col, grid) { 
+function checkValidMove(target, row, col, grid) { 
 
+    //if target is empty, return true
     if (target.value == '') {
         return true;
     }
 
+    //reset colored tiles
+    if (coloredTiles.length > 0) {
+        for (var i = 0; i < coloredTiles.length; i++) {
+            coloredTiles[i].style.backgroundColor = 'rgb(152, 152, 152)';
+        }
+        coloredTiles = [];
+    }
 
     let value = target.value;
 
+    //check if value if valid move
     for (var i = 0; i < 9; i++) {
         if (row[i].value == value && row[i] != target) {
-
+            coloredTiles.push(row[i]);
+            row[i].style.backgroundColor = 'red';
             return false;        
         }
 
         if (col[i].value == value && col[i] != target) {
+            coloredTiles.push(col[i]);
+            col[i].style.backgroundColor = 'red';
             return false; 
         }
 
         if (grid[i].value == value && grid[i] != target) {
+            coloredTiles.push(grid[i]);
+            grid[i].style.backgroundColor = 'red';
             return false; 
         }
     }
